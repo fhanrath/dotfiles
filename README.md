0d5c9a4b4b00d03b37f0c7435fadbf3544464d95
fenris <fabian.hanrath@posteo.de>
2021-12-12 20:39:38 +0100

    first simple dotfiles

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # dotfiles
+
+my dotfiles.
+
+## open source used
+
+- Nord Alacritty by Arctic Ice Studio and Sven Greb https://github.com/arcticicestudio/nord-alacritty, MIT
---
+++ link_dotfiles.sh
@@ -0,0 +1,3 @@
+ln -s "zsh/.zshrc" ~/.zshrc
+ln -s "Alacritty/.config/alacritty.yml" ~/.config/alacritty.yml
+ln -s "vscodium/.config/VSCodium/User/settings.json" ~/.config/VSCodium/User/settings.json
