81c5cb14db46404a481027fa771ffe5583193a29
fenris <fabian.hanrath@posteo.de>
2021-12-12 23:40:14 +0100

    copy instead of link

---
+++ copy_dotfiles.sh
@@ -0,0 +1,5 @@
+mkdir -p ~/.config/VSCodium/User
+
+cp zsh/.zshrc ~/.zshrc
+cp Alacritty/.config/alacritty.yml ~/.config/alacritty.yml
+cp vscodium/.config/VSCodium/User/settings.json ~/.config/VSCodium/User/settings.json
