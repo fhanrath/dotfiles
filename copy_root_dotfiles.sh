4c752214a040a71a5b71f0b0fa6f900b51ea08a4
fenris <fabian.hanrath@posteo.de>
2021-12-12 23:38:26 +0100

    retina font pipewire virtual sink

--- copy_root_dotfiles.sh
+++ copy_root_dotfiles.sh
@@ -1,1 +1,2 @@
 cp pipewire/etc/asound.conf /etc/asound.conf
+cp pipewire/etc/pipewire/virtual-stereo-sink.conf /etc/pipewire/virtual-stereo-sink.conf
--- link_dotfiles.sh
+++ link_dotfiles.sh
@@ -1,5 +1,5 @@
 mkdir -p ~/.config/VSCodium/User
 
-ln -s "zsh/.zshrc" ~/.zshrc
-ln -s "Alacritty/.config/alacritty.yml" ~/.config/alacritty.yml
-ln -s "vscodium/.config/VSCodium/User/settings.json" ~/.config/VSCodium/User/settings.json
+ln -s zsh/.zshrc ~/.zshrc
+ln -s Alacritty/.config/alacritty.yml ~/.config/alacritty.yml
+ln -s vscodium/.config/VSCodium/User/settings.json ~/.config/VSCodium/User/settings.json
