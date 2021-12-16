cbb3f15578437d7b012cc62ec7788bf1903ab856
fenris <fabian.hanrath@posteo.de>
2021-12-17 00:21:18 +0100

    load the virtual sink config

--- copy_root_dotfiles.sh
+++ copy_root_dotfiles.sh
@@ -1,2 +1,3 @@
 cp pipewire/etc/asound.conf /etc/asound.conf
 cp pipewire/etc/pipewire/virtual-stereo-sink.conf /etc/pipewire/virtual-stereo-sink.conf
+cp pipewire/usr/lib/systemd/user/pipewire_sink.service /usr/lib/systemd/user/pipewire_sink.service
