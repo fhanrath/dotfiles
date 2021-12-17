dda6ea5ae3a860f5b984b72bc97564b2d37f61e6
fenris <fabian.hanrath@posteo.de>
2021-12-17 18:38:27 +0100

    lower default sample rate

--- pipewire/create_config.sh
+++ pipewire/create_config.sh
@@ -2,7 +2,7 @@ cp /usr/share/pipewire/client.conf /etc/pipewire/client.conf
 cp /usr/share/pipewire/pipewire-pulse.conf /etc/pipewire/pipewire-pulse.conf
 cp /usr/share/pipewire/pipewire.conf /etc/pipewire/pipewire.conf
 
-sed -i 's/#default.clock.rate.*/default.clock.rate = 192000/' /etc/pipewire/pipewire.conf
+sed -i 's/#default.clock.rate.*/default.clock.rate = 96000/' /etc/pipewire/pipewire.conf
 sed -i 's/#default.clock.allowed-rates.*/default.clock.allowed-rates = [ 44100 48000 88200 96000 192000 ]/' /etc/pipewire/pipewire.conf
 
 sed -i 's/#resample.quality.*/resample.quality = 10/' /etc/pipewire/client.conf
