4a5f851a5e3731e7afd6541486feeff94bf84c8d
fenris <fabian.hanrath@posteo.de>
2022-01-01 17:53:17 +0100

    run vscode as wayland app

--- copy_root_dotfiles.sh
+++ copy_root_dotfiles.sh
@@ -1,2 +1,3 @@
 cp pipewire/etc/asound.conf /etc/asound.conf
 cp pipewire/usr/lib/systemd/user/pipewire_sink.service /usr/lib/systemd/user/pipewire_sink.service
+./vscodium/patch_desktop_file.sh
---
+++ vscodium/patch_desktop_file.sh
@@ -0,0 +1,1 @@
+sed -i "s%Exec=/opt/vscodium-bin/bin/codium --no-sandbox%Exec=/opt/vscodium-bin/bin/codium --no-sandbox --enable-features=UseOzonePlatform --ozone-platform=wayland%g" /usr/share/applications/codium.desktop
