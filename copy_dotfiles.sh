2f3fb59a5d63176012bef4c8a234c165e736485c
fenris <fabian.hanrath@posteo.de>
2022-01-05 21:55:20 +0100

    chore: refactored bat config. Use config file

--- copy_dotfiles.sh
+++ copy_dotfiles.sh
@@ -3,6 +3,7 @@ mkdir -p ~/.config/VSCodium/User
 cp zsh/.config/alias ~/.config/alias
 cp zsh/.zshrc ~/.zshrc
 cp git/.gitconfig ~/.gitconfig
+cp bat/.config/bat.conf ~/.config/bat.conf
 ./wm/copy_wm_files.sh
 cp Alacritty/.config/alacritty.yml ~/.config/alacritty.yml
 cp vscodium/.config/VSCodium/User/settings.json ~/.config/VSCodium/User/settings.json
