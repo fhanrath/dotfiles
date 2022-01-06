dde0a99b48ccd775e45ff92fdc3e01840a242038
fenris <fabian.hanrath@posteo.de>
2022-01-06 18:27:00 +0100

    chore: made patching vscode desktop file idempotent

--- vscodium/patch_desktop_file.sh
+++ vscodium/patch_desktop_file.sh
@@ -1,1 +1,4 @@
+if [ -z $(cat /usr/share/applications/codium.desktop | grep -E "platform=wayland" -) ] 
+then 
 sed -i "s%Exec=/opt/vscodium-bin/bin/codium --no-sandbox%Exec=/opt/vscodium-bin/bin/codium --no-sandbox --enable-features=UseOzonePlatform --ozone-platform=wayland%g" /usr/share/applications/codium.desktop
+fi
