3436625f951f7d7c1bdb1581774b224de12b6820
fenris <fabian.hanrath@posteo.de>
2022-01-07 23:41:54 +0100

    wip: work on gpu usage

---
+++ bin/00copy_bin.sh
@@ -0,0 +1,2 @@
+mkdir -p ~/.config/bin
+cp get_gpu_load.sh ~/.config/bin/get_gpu_load.sh
--- copy_dotfiles.sh
+++ copy_dotfiles.sh
@@ -4,6 +4,7 @@ cp zsh/.config/alias ~/.config/alias
 cp zsh/.zshrc ~/.zshrc
 cp git/.gitconfig ~/.gitconfig
 cp bat/.config/bat.conf ~/.config/bat.conf
+./bin/00copy_bin.sh
 ./wm/copy_wm_files.sh
 cp Alacritty/.config/alacritty.yml ~/.config/alacritty.yml
 cp vscodium/.config/VSCodium/User/settings.json ~/.config/VSCodium/User/settings.json
