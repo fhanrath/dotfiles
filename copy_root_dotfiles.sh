c6d294e44160db8698e2992ef957f90272249871
fenris <fabian.hanrath@posteo.de>
2022-01-07 22:46:38 +0100

    placeholder for systemd, start nm-applet onclick

--- copy_root_dotfiles.sh
+++ copy_root_dotfiles.sh
@@ -1,4 +1,5 @@
 cp pipewire/etc/asound.conf /etc/asound.conf
 ./system/copy_system_files.sh
+./systemd/copy_systemd_files.sh
 cp pipewire/usr/lib/systemd/user/pipewire_sink.service /usr/lib/systemd/user/pipewire_sink.service
 ./vscodium/patch_desktop_file.sh
