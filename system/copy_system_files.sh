784c8f456396cccd5a3519b428218c745acdf41f
fenris <fabian.hanrath@posteo.de>
2022-02-19 17:20:06 +0100

    fix: create folder

--- system/copy_system_files.sh
+++ system/copy_system_files.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 full_path=$(realpath $0)
 dir_path=$(dirname $full_path)
+
+mkdir -p /etc/NetworkManager/conf.d
  
 cp $dir_path/etc/modules-load.d/cdc_acm.conf /etc/modules-load.d/cdc_acm.conf
 cp $dir_path/etc/modules-load.d/cifs.conf /etc/modules-load.d/cifs.conf
