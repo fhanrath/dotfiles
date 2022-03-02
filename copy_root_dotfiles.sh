25cbb3b3db9b08d122efa7cadf720cb6cec0639a
fenris <fabian.hanrath@posteo.de>
2022-03-02 23:06:13 +0100

    fix: don't patch the /usr/ file

--- copy_root_dotfiles.sh
+++ copy_root_dotfiles.sh
@@ -5,4 +5,3 @@ cp pipewire/etc/asound.conf /etc/asound.conf
 ./systemd/copy_systemd_files.sh
 cp pipewire/usr/lib/systemd/user/pipewire_sink.service /usr/lib/systemd/user/pipewire_sink.service
 cp /usr/share/backgrounds/sway/Sway_Wallpaper_Blue_1920x1080.png /usr/share/backgrounds/fenris/desktop_wallpaper
-./vscodium/patch_desktop_file.sh
