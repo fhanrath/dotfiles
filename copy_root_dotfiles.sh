5ae4b69b40597655a67f2b535aa89fbf0109e0a7
fenris <fabian.hanrath@posteo.de>
2022-03-02 23:02:30 +0100

    fix: set default wallpaper

--- copy_root_dotfiles.sh
+++ copy_root_dotfiles.sh
@@ -4,4 +4,5 @@ cp pipewire/etc/asound.conf /etc/asound.conf
 ./system/copy_system_files.sh
 ./systemd/copy_systemd_files.sh
 cp pipewire/usr/lib/systemd/user/pipewire_sink.service /usr/lib/systemd/user/pipewire_sink.service
+cp /usr/share/backgrounds/sway/Sway_Wallpaper_Blue_1920x1080.png /usr/share/backgrounds/fenris/desktop_wallpaper
 ./vscodium/patch_desktop_file.sh
