4fd34525228f7c8eba8a630f7004fb5882bfa1e4
fenris <fabian.hanrath@posteo.de>
2022-03-03 19:38:53 +0100

    fix: add config for screen sharing

--- pipewire/create_config.sh
+++ pipewire/create_config.sh
@@ -1,9 +1,14 @@
+mkdir -p /etc/pipewire/media-session.d
+
 cp /usr/share/pipewire/client.conf /etc/pipewire/client.conf
 cp /usr/share/pipewire/pipewire-pulse.conf /etc/pipewire/pipewire-pulse.conf
 cp /usr/share/pipewire/pipewire.conf /etc/pipewire/pipewire.conf
+cp /usr/share/pipewire/media-session.d/media-session.conf /etc/pipewire/media-session.d/media-session.conf
 
 sed -i 's/#default.clock.rate.*/default.clock.rate = 96000/' /etc/pipewire/pipewire.conf
 sed -i 's/#default.clock.allowed-rates.*/default.clock.allowed-rates = [ 44100 48000 88200 96000 192000 ]/' /etc/pipewire/pipewire.conf
 
 sed -i 's/#resample.quality.*/resample.quality = 10/' /etc/pipewire/client.conf
 sed -i 's/#resample.quality.*/resample.quality = 10/' /etc/pipewire/pipewire-pulse.conf
+
+sed -i 's/#metadata/metadata/' /etc/pipewire/media-session.d/media-session.conf
