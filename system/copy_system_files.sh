74c2c13cefbf85b2f11c2c8fc3888e2927d4c858
--global <--global>
2022-03-29 21:23:21 +0200

    fix: environment

--- system/copy_system_files.sh
+++ system/copy_system_files.sh
@@ -6,5 +6,5 @@ mkdir -p /etc/NetworkManager/conf.d
  
 cp $dir_path/etc/modules-load.d/cdc_acm.conf /etc/modules-load.d/cdc_acm.conf
 cp $dir_path/etc/modules-load.d/cifs.conf /etc/modules-load.d/cifs.conf
-cp $dir_path/etc/environment /etc/environments
+cp $dir_path/etc/environment /etc/environment
 cp $dir_path/etc/NetworkManager/conf.d/ip6-privacy.conf /etc/NetworkManager/conf.d/ip6-privacy.conf
